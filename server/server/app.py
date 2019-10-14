88b94faa393243b466b148dc8507c005f059bb4d
danalizieors <danalizieors@gmail.com>
2019-10-14 16:55:46 +0300

    Flask global variable test

--- server/server/app.py
+++ server/server/app.py
@@ -2,11 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+cache = {
+    "data": 0
+}
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/method1')
+def method1():
+    cache["data"] += 1
+    return str(cache["data"])
 
+@app.route('/method2')
+def method2():
+    cache["data"] -= 1
+    return str(cache["data"])
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8080)

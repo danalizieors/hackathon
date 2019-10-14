4613342e9627a5bf8db40504de93158eaca446e5
danalizieors <danalizieors@gmail.com>
2019-10-14 18:33:51 +0300

    Routing separated in a different file

--- server/server/app.py
+++ server/server/app.py
@@ -1,20 +1,8 @@
 from flask import Flask
+from routes import route
 
 app = Flask(__name__)
-
-cache = {
-    "data": 0
-}
-
-@app.route('/method1')
-def method1():
-    cache["data"] += 1
-    return str(cache["data"])
-
-@app.route('/method2')
-def method2():
-    cache["data"] -= 1
-    return str(cache["data"])
+route(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000)

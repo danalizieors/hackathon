996e24033a5da33305606c0572a3594c16b3f133
danalizieors <danalizieors@gmail.com>
2019-10-19 12:52:17 +0300

    Database schema created

--- server/server/learning/real/predictor.py
+++
@@ -1,2 +0,0 @@
-def predict(post):
-    return post
--- server/server/persistence/database.py
+++ server/server/persistence/database.py
@@ -5,17 +5,35 @@ from server.persistence.PanDatabase import PanDatabase
 
 class Database(PanDatabase):
     def __init__(self):
+        self.users = columns(
+
+            # identification
+            'id',
+
+            # attributes
+            'sex',
+            'year_of_birth',
+            'height',
+            'weight',
+            'fitness',
+            'pulse',
+            'sleep',
+            'noise',
+            'home_location',
+
+            # preferences
+            'in_temperature',
+            'in_humidity',
+        )
+
         self.posts = columns(
 
             # identification
-            'id',  # automatically set
-            'user_id',  # sent in request
+            'id',
+            'user_id',
 
             # user defined
-            'type',  # acute | chronic
-            'symptom',  # selected or defined by user
-            'affected',  # is the user currently affected
-            'notice',  # user's observations
+            'symptom_id',
 
             # phone
             'time',
@@ -32,6 +50,40 @@ class Database(PanDatabase):
             'in_humidity',
             'in_dust',
             'in_noise',
+
+            # wearable
+            'fitness',
+            'pulse',
+            'sleep',
+        )
+
+        self.links = columns(
+
+            # identification
+            'post_id_symptom',
+            'post_id_solution',
+        )
+
+        self.symptoms = columns(
+
+            # identification
+            'id',
+            'user_id',
+
+            # attributes
+            'severity',
+            'suggestion_id',
+        )
+
+        self.suggestions = columns(
+
+            # identification
+            'id',
+            'user_id',
+
+            # attributes
+            'text'
+            'score',
         )
 
 
@@ -40,5 +92,5 @@ def columns(*names):
 
 
 DATABASE = Database()
-DATABASE.load('../../database/mock')
+DATABASE.load('../../database/')
 print(DATABASE)
